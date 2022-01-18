1fc0c335823b5a6a8b65cfaf07603a00cbbc2c6a
Mol0k <cristiansmurfcsgo@gmail.com>
2022-01-18 18:41:03 +0100

    Update categoria.js categoria.html

--- js/categoria.js
+++ js/categoria.js
@@ -1,7 +1,7 @@
-function categoria() {
+function calcularCategoria() {
     let fecha = document.getElementById('fecha')
     // console.log(fecha.value)
-    let resultado = document.getElementsByClassName('resultado')
+    var resultado = document.getElementsByClassName('resultado')
     let array = fecha.value.split('-')
     // console.log(array)
     let hoy = new Date()
@@ -9,9 +9,27 @@ function categoria() {
     // console.log(fechaNacimiento)
     let edad = hoy.getTime() - fechaNacimiento.getTime()
     edad = edad / (1000 * 60 * 60 * 24 * 365)
-    if (edad > 3 && edad <= 9) {
+    // console.log(edad)
+    
+    for (var i = 0; i < resultado.length; i++) {
+        resultado[i].style.fontWeight = 'normal'
+    }
+
+    if (edad <= 6) {
         resultado[0].style.fontWeight = 'bold'
-    } else if (edad > 9 && edad <= 15) {
-    } else if (edad > 15 && edad <= 20) {
+    } else if (edad >= 7 && edad <= 8) {
+        resultado[1].style.fontWeight = 'bold'
+    } else if (edad >= 9 && edad <= 10) {
+        resultado[2].style.fontWeight = 'bold'
+    } else if (edad >= 11 && edad <= 12) {
+        resultado[3].style.fontWeight = 'bold'
+    } else if (edad >= 13 && edad <= 14) {
+        resultado[4].style.fontWeight = 'bold'
+    } else if (edad > 15 && edad <= 16) {
+        resultado[5].style.fontWeight = 'bold'
+    } else if (edad > 17 && edad <= 18) {
+        resultado[6].style.fontWeight = 'bold'
+    } else if (edad > 19) {
+        resultado[7].style.fontWeight = 'bold'
     }
 }
