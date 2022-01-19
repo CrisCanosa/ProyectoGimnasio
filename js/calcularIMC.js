7e89f63382a94235f0d56e7dbdbb442cf5b424c6
Mol0k <cristiansmurfcsgo@gmail.com>
2022-01-19 19:44:01 +0100

    Create rama_de_prueba

---
+++ js/calcularIMC.js
@@ -0,0 +1,56 @@
+document.getElementById('resultado').style.display = 'none'
+
+function calcularIMC() {
+    var peso, altura, imc, estado
+    //hacemos la llamada a los datos introducidos
+    peso = document.getElementById('peso').value
+    altura = document.getElementById('altura').value / 100
+    //calculamos el imc
+    imc = peso / (altura * altura)
+    //enviamos el resultado a la caja correspondiente y lo reducimos a 2 decimales
+    document.getElementById('imc').value = imc.toFixed(2)
+    document.getElementById('resultado').style.display = 'block'
+
+    if (peso != '' && altura != '') {
+        if (imc <= 16) {
+            estado = 'Delgadez severa'
+            // estado = `Delgadez severa ${(altura * altura * 16 - peso).toFixed(1)} kilos`
+        } else if (imc >= 16 && imc <= 16.99) {
+            estado = 'Delgadez moderada'
+        } else if (imc >= 17 && imc <= 18.49) {
+            estado = 'Delgadez aceptable'
+        } else if (imc >= 18.5 && imc <= 24.99) {
+            estado = 'Peso normal'
+        } else if (imc >= 25 && imc <= 29.99) {
+            estado = 'Sobrepeso'
+        } else if (imc >= 30 && imc <= 34.99) {
+            estado = 'Obeso Tipo I'
+        } else if (imc >= 35 && imc <= 40) {
+            estado = 'Obesidad tipo II'
+        } else if (imc > 40) {
+            estado = 'Obesidad tipo III'
+        }
+    }
+    document.getElementById('estado').value = estado
+    
+    /*VALIDACIONES*/
+    var fPeso = document.forms['miForm']['peso']
+    if (fPeso.value == '') {
+        alert('Introduce tu peso:')
+        document.getElementById('resultado').style.display = 'none'
+        return
+    }
+    var fAltura = document.forms['miForm']['altura']
+    if (fAltura.value == '') {
+        alert('Introduce tu altura:')
+        document.getElementById('resultado').style.display = 'none'
+        return
+    }
+}
+
+function limpiar() {
+    document.getElementById('miForm').reset()
+    document.getElementById('resultado').style.display = 'none'
+
+    console.log('Estoy ejecutando clear')
+}
---
+++ js/categoria.js
@@ -0,0 +1,35 @@
+function calcularCategoria() {
+    let fecha = document.getElementById('fecha')
+    // console.log(fecha.value)
+    var resultado = document.getElementsByClassName('resultado')
+    let array = fecha.value.split('-')
+    // console.log(array)
+    let hoy = new Date()
+    let fechaNacimiento = new Date(array[0], Number(array[1]) - 1, array[2])
+    // console.log(fechaNacimiento)
+    let edad = hoy.getTime() - fechaNacimiento.getTime()
+    edad = edad / (1000 * 60 * 60 * 24 * 365)
+    // console.log(edad)
+
+    for (var i = 0; i < resultado.length; i++) {
+        resultado[i].style.fontWeight = 'normal'
+    }
+
+    if (edad <= 6) {
+        resultado[0].style.fontWeight = 'bold'
+    } else if (edad >= 7 && edad <= 8) {
+        resultado[1].style.fontWeight = 'bold'
+    } else if (edad >= 9 && edad <= 10) {
+        resultado[2].style.fontWeight = 'bold'
+    } else if (edad >= 11 && edad <= 12) {
+        resultado[3].style.fontWeight = 'bold'
+    } else if (edad >= 13 && edad <= 14) {
+        resultado[4].style.fontWeight = 'bold'
+    } else if (edad > 15 && edad <= 16) {
+        resultado[5].style.fontWeight = 'bold'
+    } else if (edad > 17 && edad <= 18) {
+        resultado[6].style.fontWeight = 'bold'
+    } else if (edad > 19) {
+        resultado[7].style.fontWeight = 'bold'
+    }
+}
