f6c45f6379601cd44500e95be7885464824d75b5
Mol0k <80773949+Mol0k@users.noreply.github.com>
2022-01-20 14:26:16 +0100

    Update categoria.js

--- js/categoria.js
+++ js/categoria.js
@@ -9,7 +9,7 @@ function calcularCategoria() {
     // console.log(fechaNacimiento)
     let edad = hoy.getTime() - fechaNacimiento.getTime()
     edad = edad / (1000 * 60 * 60 * 24 * 365)
-    // console.log(edad)
+    console.log(edad)
 
     for (var i = 0; i < resultado.length; i++) {
         resultado[i].style.fontWeight = 'normal'
@@ -17,19 +17,19 @@ function calcularCategoria() {
 
     if (edad <= 6) {
         resultado[0].style.fontWeight = 'bold'
-    } else if (edad >= 7 && edad <= 8) {
+    } else if (edad >= 7 && edad < 9) {
         resultado[1].style.fontWeight = 'bold'
-    } else if (edad >= 9 && edad <= 10) {
+    } else if (edad >= 9 && edad < 11) {
         resultado[2].style.fontWeight = 'bold'
-    } else if (edad >= 11 && edad <= 12) {
+    } else if (edad >= 11 && edad < 13) {
         resultado[3].style.fontWeight = 'bold'
-    } else if (edad >= 13 && edad <= 14) {
+    } else if (edad >= 13 && edad < 15) {
         resultado[4].style.fontWeight = 'bold'
-    } else if (edad > 15 && edad <= 16) {
+    } else if (edad >= 15 && edad < 17) {
         resultado[5].style.fontWeight = 'bold'
-    } else if (edad > 17 && edad <= 18) {
+    } else if (edad >= 17 && edad < 19) {
         resultado[6].style.fontWeight = 'bold'
-    } else if (edad > 19) {
+    } else if (edad >= 19) {
         resultado[7].style.fontWeight = 'bold'
     }
 }
