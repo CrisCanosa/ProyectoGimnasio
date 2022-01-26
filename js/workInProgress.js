328915d1fed37f8e5bc11300494e03ce06009df0
Mol0k <80773949+Mol0k@users.noreply.github.com>
2022-01-26 12:24:26 +0100

    Commit

--- js/workInProgress.js
+++ js/workInProgress.js
@@ -3,7 +3,7 @@ function guardar() {
     let cat = document.getElementById('cat').value
     let precio = document.getElementById('precio').value
     let stock = document.getElementById('stock').value
-
+    let eliminar = document.getElementsByTagName('bot_elim')
     let fila =
         '<tr><td>' +
         nom +
@@ -13,10 +13,16 @@ function guardar() {
         precio +
         '</td><td>' +
         stock +
+        '</td><td>' +
+        eliminar + 
         '</td></tr>'
 
     let btn = document.createElement('TR')
     btn.innerHTML = fila
     document.getElementById('tabla').appendChild(btn)
-    
+
 }
+// function Eliminar(i) {
+//     document.getElementsByTagName("table")[0].setAttribute("id", "tableid");
+//     document.getElementById("tableid").deleteRow(i);
+// }
